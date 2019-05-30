83b7125e3f4d09edfe16ec532eea321ab9d8ec93
at1208 <amantiwari12081999@gmail.com>
2019-05-30 21:45:54 +0530

    styling added

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,36 +6,49 @@ const About = () => {
          <div className='container-fluid row col-lg-12'>
          <div class="h1 col-lg-2">
          <img className="h3 shadow card-img-top" src="aman.jpg"  alt='' />
+
        </div>
 
-         <div className='h2 col-lg-6 card '>
+         <div className='h2 col-lg-6 card shadow '>
+
+               <div className="progress h7 h5">
+                <h1 className='h6'>Html</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '70%'}} ariaValuenow="70" ariaValuemin="0" ariaaluemax="100">70%</div>
+               </div>
+
 
             <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '70%'}} ariaValuenow="70" ariaValuemin="0" ariaaluemax="100">html 70%</div>
+             <h1 className='h6'>Css</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '60%'}} ariaValuenow="60" ariaValuemin="0" ariaaluemax="100">60%</div>
             </div>
+
+
+
             <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '60%'}} ariaValuenow="60" ariaValuemin="0" ariaaluemax="100">css 60%</div>
+             <h1 className='h6'>Javascript</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '60%'}} ariaValuenow="60" ariaValuemin="0" ariaaluemax="100">  60%</div>
             </div>
+
             <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '70%'}} ariaValuenow="70" ariaValuemin="0" ariaaluemax="100">bootstrap 70%</div>
+             <h1 className='h6'>React</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '80%'}} ariaValuenow="80" ariaValuemin="0" ariaaluemax="100"> 80%</div>
             </div>
+
+
+
             <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '80%'}} ariaValuenow="80" ariaValuemin="0" ariaaluemax="100">React 80%</div>
-            </div>
-            <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '60%'}} ariaValuenow="60" ariaValuemin="0" ariaaluemax="100">Redux 60%</div>
-            </div>
-            <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '50%'}} ariaValuenow="50" ariaValuemin="0" ariaaluemax="100">NodeJS 50%</div>
+             <h1 className='h6'>Redux</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '50%'}} ariaValuenow="50" ariaValuemin="0" ariaaluemax="100"> 50%</div>
              </div>
+
+
              <div className="progress h5">
-              <div className="progress-bar" role="progressbar" style={{width: '60%'}} ariaValuenow="60" ariaValuemin="0" ariaaluemax="100">javascript 60%</div>
+              <h1 className='h6'>Node</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '60%'}} ariaValuenow="60" ariaValuemin="0" ariaaluemax="100">  60%</div>
             </div>
+
+
             <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '50%'}} ariaValuenow="50" ariaValuemin="0" ariaaluemax="100">ExpressJS 50%</div>
+             <h1 className='h6'>Express</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '50%'}} ariaValuenow="50" ariaValuemin="0" ariaaluemax="100"> 50%</div>
             </div>
+
+
             <div className="progress h5">
-             <div className="progress-bar" role="progressbar" style={{width: '30%'}} ariaValuenow="30" ariaValuemin="0" ariaaluemax="100">MongoDB 30%</div>
+             <h1 className='h6'>MongoDB</h1><div className="progress-bar bg-info" role="progressbar" style={{width: '20%'}} ariaValuenow="20" ariaValuemin="0" ariaaluemax="100">  20%</div>
             </div>
 
          </div>
