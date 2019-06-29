3fe16a7e278c138c356472b3b54a48ff75c35018
aman tiwari <amantiwari12081999@gmail.com>
2019-06-29 19:55:57 +0530

    image added to card

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './portfolio.css'
 
 const About = () => {
-  return <div className='u1 container shadow card' >
-
+  return <div className='container-fluid' >
+         <div className='row'>
+              <div className='col-lg shadow container u1'>
+                <img className='card-img-top i1' src='chillax.png' alt=''/>
+              </div>
+              <div className='col-lg  shadow container u1'>
+              </div>
+              <div className='col-lg shadow container u1'>
+              </div>
+              <div className='col-lg  shadow container u1'>
+              </div>
+         </div>
          </div>
 }
 export default About;
