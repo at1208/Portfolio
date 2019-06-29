939044e4ceaab46536d40357f7ba66ef11e6b448
aman tiwari <amantiwari12081999@gmail.com>
2019-06-29 20:18:57 +0530

    more images added

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -4,14 +4,17 @@ import './portfolio.css'
 const About = () => {
   return <div className='container-fluid' >
          <div className='row'>
-              <div className='col-lg shadow container u1'>
-                <img className='card-img-top i1' src='chillax.png' alt=''/>
+              <div className='col-lg card shadow container  u1'>
+                <img className='container card-img-top i1' src='chillax.png' alt=''/>
               </div>
-              <div className='col-lg  shadow container u1'>
+              <div className='col-lg card shadow container u1'>
+                <img className='container  card-img-top i1' src='searchbar.png' alt=''/>
               </div>
-              <div className='col-lg shadow container u1'>
+              <div className='col-lg shadow container card u1'>
+                <img className='container  card-img-top i1' src='shopnow.png' alt=''/>
               </div>
-              <div className='col-lg  shadow container u1'>
+              <div className='col-lg  shadow container-fluid card u1'>
+                <img className='container  card-img-top i1' src='changelang.png' alt=''/>
               </div>
          </div>
          </div>
