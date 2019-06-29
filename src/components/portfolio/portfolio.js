3fccceff3bc93c82f9cf6a33ab3109173eaab2df
aman tiwari <amantiwari12081999@gmail.com>
2019-06-29 20:44:22 +0530

    change styling changed

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -4,17 +4,26 @@ import './portfolio.css'
 const About = () => {
   return <div className='container-fluid' >
          <div className='row'>
-              <div className='col-lg card shadow container  u1'>
+              <div className='col-lg-3 card  container  u1'>
                 <img className='container card-img-top i1' src='chillax.png' alt=''/>
               </div>
-              <div className='col-lg card shadow container u1'>
+              <div className='col-lg-3  card container u1'>
                 <img className='container  card-img-top i1' src='searchbar.png' alt=''/>
               </div>
-              <div className='col-lg shadow container card u1'>
+              <div className='col-lg-3 card container u1'>
                 <img className='container  card-img-top i1' src='shopnow.png' alt=''/>
               </div>
-              <div className='col-lg  shadow container-fluid card u1'>
+              <div className='col-lg-3 card container-fluid u1'>
                 <img className='container  card-img-top i1' src='changelang.png' alt=''/>
+              </div>
+              <div className='col-lg-3 card container-fluid u1'>
+                <img className='container  card-img-top i1' src='winsum.png' alt=''/>
+              </div>
+              <div className='col-lg-3 card container-fluid u1'>
+                <img className='container  card-img-top i1' src='dailynews.png' alt=''/>
+              </div>
+              <div className='col-lg-3  card container-fluid  u1'>
+                <img className='container card-img-top i1' src='weather.png' alt=''/>
               </div>
          </div>
          </div>
