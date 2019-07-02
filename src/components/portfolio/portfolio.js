c757a6a86463f77dc9d6293e1a6e25a43294f7d7
aman tiwari <amantiwari12081999@gmail.com>
2019-07-02 19:13:20 +0530

    fixed responsiveness

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -6,24 +6,42 @@ const About = () => {
          <div className='row'>
               <div className='col-lg-3 card  container  u1'>
                 <img className='container card-img-top i1' src='chillax.png' alt=''/>
+
+                <div className='container shadow k1'>
+
+                </div>
+
               </div>
-              <div className='col-lg-3  card container u1'>
+              <div className='col-lg-3 card container u1'>
                 <img className='container  card-img-top i1' src='searchbar.png' alt=''/>
+                <div className='container shadow k1'>
+
+                </div>
               </div>
               <div className='col-lg-3 card container u1'>
                 <img className='container  card-img-top i1' src='shopnow.png' alt=''/>
+                <div className='container shadow k1'>
+
+                </div>
               </div>
               <div className='col-lg-3 card container-fluid u1'>
                 <img className='container  card-img-top i1' src='changelang.png' alt=''/>
+                <div className='container shadow k1'>
+
+                </div>
               </div>
               <div className='col-lg-3 card container-fluid u1'>
                 <img className='container  card-img-top i1' src='winsum.png' alt=''/>
+                <div className='container shadow k1'>
+
+                </div>
               </div>
-              <div className='col-lg-3 card container-fluid u1'>
-                <img className='container  card-img-top i1' src='dailynews.png' alt=''/>
-              </div>
+
               <div className='col-lg-3  card container-fluid  u1'>
                 <img className='container card-img-top i1' src='weather.png' alt=''/>
+                <div className='container shadow k1'>
+
+                </div>
               </div>
          </div>
          </div>
