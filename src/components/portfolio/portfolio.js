dcef10776a3d09e133ca479a4d516fdc63664481
aman tiwari <amantiwari12081999@gmail.com>
2019-07-02 21:34:02 +0530

    released date added

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -7,45 +7,99 @@ const About = () => {
 
               <div className='col-lg-3 shadow container text-center u1'>
                 <img className='i1' src='chillax.png' alt=''/>
-               <div className='container text-center shadow k1'>
+               <div className='container text-center k1'>
                   <h1 className='v1' >Chillax</h1>
                   <hr />
+                 <div className='container'>
+                     <span  className='container shadow col-xm l1'>
+                      <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                      First Released
+                     </span>
+                     <span  className='container shadow col-xm l1'>
+                     13 May 2018
+                      </span>
+                 </div>
                 </div>
               </div>
               <div className='col-lg-3 shadow container  text-center u1'>
                 <img className='   i1' src='searchbar.png' alt=''/>
-                <div className='container text-center shadow k1'>
+                <div className='container text-center  k1'>
                  <h1 className='v1'>SearchBar</h1>
                    <hr />
+                   <div className='container'>
+                       <span  className='container shadow col-xm l1'>
+                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                        First Released
+                       </span>
+                       <span  className='container shadow col-xm l1'>
+                       18 May 2018
+                        </span>
+                   </div>
                 </div>
               </div>
               <div className='col-lg-3 shadow container  text-center u1'>
                 <img className='    i1' src='shopnow.png' alt=''/>
-                <div className='container text-center shadow k1'>
+                <div className='container text-center  k1'>
                 <h1 className='v1'>Shopping</h1>
                   <hr />
+                  <div className='container'>
+                      <span  className='container shadow col-xm l1'>
+                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                       First Released
+                      </span>
+                      <span  className='container shadow col-xm l1'>
+                      31 May 2018
+                       </span>
+                  </div>
                 </div>
               </div>
               <div className='col-lg-3 shadow container   text-center u1'>
-                <img className='      i1' src='changelang.png' alt=''/>
-                <div className='container text-center shadow k1'>
+                <img className='     i1' src='changelang.png' alt=''/>
+                <div className='container text-center  k1'>
                  <h1 className='v1'>Change Lang</h1>
                    <hr />
+                   <div className='container'>
+                       <span  className='container shadow col-xm l1'>
+                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                        First Released
+                       </span>
+                       <span  className='container shadow col-xm l1'>
+                       24 May 2018
+                        </span>
+                   </div>
                 </div>
               </div>
               <div className='col-lg-3 shadow container   text-center u1'>
                 <img className='     i1' src='winsum.png' alt=''/>
-                <div className='container text-center shadow k1'>
+                <div className='container text-center  k1'>
                 <h1 className='v1'>Season</h1>
                   <hr />
+                  <div className='container'>
+                      <span  className='container shadow col-xm l1'>
+                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                       First Released
+                      </span>
+                      <span  className='container shadow col-xm l1'>
+                      18 May 2018
+                       </span>
+                  </div>
                 </div>
               </div>
 
               <div className='col-lg-3  shadow container  text-center u1'>
                 <img className='    i1' src='weather.png' alt=''/>
-                <div className='container text-center shadow k1'>
-                  <h1 className='v1'>Weather</h1>
+                <div className='container text-center   k1'>
+                  <h1 className='v1'>Weather Finder</h1>
                     <hr />
+                    <div className='container'>
+                        <span  className='container shadow col-xm l1'>
+                         <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                         First Released
+                        </span>
+                        <span  className='container shadow col-xm l1'>
+                        13 Feb 2018
+                         </span>
+                    </div>
                 </div>
               </div>
          </div>
