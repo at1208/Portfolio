f564a7e5390afe47636e735cdafd4bfb15f79777
aman tiwari <amantiwari12081999@gmail.com>
2019-07-03 16:44:15 +0530

    some style improved

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -7,22 +7,22 @@ const About = () => {
          <div className='row justify-content-center'>
 
               <div className='col-lg-3 shadow container text-center u1 o1'>
-                <img className='i1' src='chillax.png' alt=''/>
+                <img className='i1' src=' ' alt=''/>
                <div className='container text-center k1'>
                   <h1 className='v1' >Chillax</h1>
                   <hr />
-                    <div className='shadow p2 '>
+                    <div className='shadow p2 o1'>
                       <a  href='https://chillax.netlify.com'>Live</a>
                     </div>
                  <div className='container'>
                      <span  className='container shadow l1'>
-                      <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue' }}></i>
+                      <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: ' Chartreuse' }}></i>
                       First Released
                      </span>
                      <span  className='container shadow   l1' style={{  fontWeight:'bold' }}>
                      13 May 2018
                      </span>
-                    <div className='container shadow l2 '><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Chillax'>https://github.com/at1208/Chillax</a></div>
+                    <div className='container shadow l2 n1'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Chillax'>https://github.com/at1208/Chillax</a></div>
                  </div>
 
                 </div>
@@ -30,110 +30,110 @@ const About = () => {
 
 
               <div className='col-lg-3 shadow container  text-center u1 o2'>
-                <img className='   i1' src='searchbar.png' alt=''/>
+                <img className='   i1' src=' ' alt=''/>
                 <div className='container text-center  k1'>
                  <h1 className='v1'>SearchBar</h1>
                    <hr />
-                   <div className='shadow p2'>
+                   <div className='shadow p2 o2'>
                   <a href='https://searchkaro.netlify.com'>Live</a>
                    </div>
                    <div className='container'>
                        <span  className='container shadow col-xm l1'>
-                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'Aqua'}}></i>
                         First Released
                        </span>
                        <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                        18 May 2018
                         </span>
-                          <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/SearchBar'>https://github.com/at1208/SearchBar</a></div>
+                          <div className='container shadow l2 n1'> <i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/SearchBar'>https://github.com/at1208/SearchBar</a> </div>
                    </div>
                 </div>
               </div>
 
 
               <div className='col-lg-3 shadow container  text-center u1 o3'>
-                <img className='    i1' src='shopnow.png' alt=''/>
+                <img className='    i1' src=' ' alt=''/>
                 <div className='container text-center  k1'>
                 <h1 className='v1'>Shopping</h1>
                   <hr />
-                  <div className='shadow p2'>
+                  <div className='shadow p2 o3'>
                   <a   href='https://shopsnow.netlify.com'>Live</a>
                   </div>
                   <div className='container'>
                       <span  className='container shadow col-xm l1'>
-                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'DeepPink'}}></i>
                        First Released
                       </span>
                       <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                       31 May 2018
                        </span>
-                         <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Shopping'>https://github.com/at1208/Shopping</a></div>
+                         <div className='container shadow n1 l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Shopping'>https://github.com/at1208/Shopping</a></div>
                   </div>
                 </div>
               </div>
 
 
               <div className='col-lg-3 shadow container   text-center u1 o4'>
-                <img className='     i1' src='changelang.png' alt=''/>
+                <img className='     i1' src=' ' alt=''/>
                 <div className='container text-center  k1'>
                  <h1 className='v1'>Change Lang</h1>
                    <hr />
-                   <div className='shadow p2'>
+                   <div className='shadow p2 o4'>
                     <a  href='https://changelang.netlify.com'>Live</a>
                    </div>
                    <div className='container'>
                        <span  className='container shadow col-xm l1'>
-                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'Red'}}></i>
                         First Released
                        </span>
                        <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                        24 May 2018
                         </span>
-                          <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/LangSelector'>https://github.com/at1208/LangSelector</a></div>
+                          <div className='container shadow n1 l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/LangSelector'>https://github.com/at1208/LangSelector</a></div>
                    </div>
                 </div>
               </div>
 
 
               <div className='col-lg-3 shadow container   text-center u1 o5'>
-                <img className='     i1' src='winsum.png' alt=''/>
+                <img className='     i1' src=' ' alt=''/>
                 <div className='container text-center  k1'>
                 <h1 className='v1'>Season</h1>
                   <hr />
-                  <div className='shadow p2'>
+                  <div className='shadow p2 o5'>
                   <a   href='https://winsum.netlify.com'>Live</a>
                   </div>
                   <div className='container'>
                       <span  className='container shadow col-xm l1'>
-                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'Yellow'}}></i>
                        First Released
                       </span>
                       <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                       18 May 2018
                        </span>
-                         <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Geolocation'>https://github.com/at1208/Geolocation</a></div>
+                         <div className='container shadow n1 l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Geolocation'>https://github.com/at1208/Geolocation</a></div>
                   </div>
                 </div>
               </div>
 
 
               <div className='col-lg-3  shadow container  text-center u1 o6'>
-                <img className='    i1' src='weather.png' alt=''/>
+                <img className='    i1' src=' ' alt=''/>
                 <div className='container text-center   k1'>
                   <h1 className='v1'>Weather Finder</h1>
                     <hr />
-                    <div className='shadow p2'>
+                    <div className='shadow p2 o6'>
                     <a   href=' '>Live</a>
                     </div>
                     <div className='container'>
                         <span  className='container shadow col-xm l1'>
-                         <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
+                         <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'DodgerBlue'}}></i>
                          First Released
                         </span>
                         <span  className='container shadow col-xm l1' >
                         13 Feb 2018
                          </span>
-                           <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Weather-finder'>https://github.com/at1208/Weather-finder</a></div>
+                           <div className='container shadow n1 l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Weather-finder'>https://github.com/at1208/Weather-finder</a></div>
                     </div>
                 </div>
               </div>
