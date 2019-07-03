89151d2a7dbe942ac43a556d4c421fcd98b6bb81
aman tiwari <amantiwari12081999@gmail.com>
2019-07-03 11:51:47 +0530

    live site link added

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -4,13 +4,16 @@ import './portfolio.css'
 
 const About = () => {
   return <div className='container-fluid' >
-         <div className='row justify-content-center '>
+         <div className='row justify-content-center'>
 
               <div className='col-lg-3 shadow container text-center u1'>
                 <img className='i1' src='chillax.png' alt=''/>
                <div className='container text-center k1'>
                   <h1 className='v1' >Chillax</h1>
                   <hr />
+                    <div className='shadow p2 '>
+                      <a  href='https://chillax.netlify.com'>Live</a>
+                    </div>
                  <div className='container'>
                      <span  className='container shadow   l1'>
                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue' }}></i>
@@ -24,11 +27,16 @@ const About = () => {
 
                 </div>
               </div>
+
+
               <div className='col-lg-3 shadow container  text-center u1'>
                 <img className='   i1' src='searchbar.png' alt=''/>
                 <div className='container text-center  k1'>
                  <h1 className='v1'>SearchBar</h1>
                    <hr />
+                   <div className='shadow p2'>
+                  <a href='https://searchkaro.netlify.com'>Live</a>
+                   </div>
                    <div className='container'>
                        <span  className='container shadow col-xm l1'>
                         <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
@@ -41,11 +49,16 @@ const About = () => {
                    </div>
                 </div>
               </div>
+
+
               <div className='col-lg-3 shadow container  text-center u1'>
                 <img className='    i1' src='shopnow.png' alt=''/>
                 <div className='container text-center  k1'>
                 <h1 className='v1'>Shopping</h1>
                   <hr />
+                  <div className='shadow p2'>
+                  <a   href='https://shopsnow.netlify.com'>Live</a>
+                  </div>
                   <div className='container'>
                       <span  className='container shadow col-xm l1'>
                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
@@ -58,11 +71,16 @@ const About = () => {
                   </div>
                 </div>
               </div>
+
+
               <div className='col-lg-3 shadow container   text-center u1'>
                 <img className='     i1' src='changelang.png' alt=''/>
                 <div className='container text-center  k1'>
                  <h1 className='v1'>Change Lang</h1>
                    <hr />
+                   <div className='shadow p2'>
+                    <a  href='https://changelang.netlify.com'>Live</a>
+                   </div>
                    <div className='container'>
                        <span  className='container shadow col-xm l1'>
                         <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
@@ -75,11 +93,16 @@ const About = () => {
                    </div>
                 </div>
               </div>
+
+
               <div className='col-lg-3 shadow container   text-center u1'>
                 <img className='     i1' src='winsum.png' alt=''/>
                 <div className='container text-center  k1'>
                 <h1 className='v1'>Season</h1>
                   <hr />
+                  <div className='shadow p2'>
+                  <a   href='https://winsum.netlify.com'>Live</a>
+                  </div>
                   <div className='container'>
                       <span  className='container shadow col-xm l1'>
                        <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
@@ -93,17 +116,21 @@ const About = () => {
                 </div>
               </div>
 
+
               <div className='col-lg-3  shadow container  text-center u1'>
                 <img className='    i1' src='weather.png' alt=''/>
                 <div className='container text-center   k1'>
                   <h1 className='v1'>Weather Finder</h1>
                     <hr />
+                    <div className='shadow p2'>
+                    <a   href=' '>Live</a>
+                    </div>
                     <div className='container'>
                         <span  className='container shadow col-xm l1'>
                          <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue'}}></i>
                          First Released
                         </span>
-                        <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
+                        <span  className='container shadow col-xm l1' >
                         13 Feb 2018
                          </span>
                            <div className='container shadow l2'><i className='fa fa-github' /> <a href='https://github.com/at1208/Weather-finder'>https://github.com/at1208/Weather-finder</a></div>
