3866c44b403137de0f4d868cb097d9540d8a2a83
aman tiwari <amantiwari12081999@gmail.com>
2019-07-03 16:08:53 +0530

    detail added

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -6,7 +6,7 @@ const About = () => {
   return <div className='container-fluid' >
          <div className='row justify-content-center'>
 
-              <div className='col-lg-3 shadow container text-center u1'>
+              <div className='col-lg-3 shadow container text-center u1 o1'>
                 <img className='i1' src='chillax.png' alt=''/>
                <div className='container text-center k1'>
                   <h1 className='v1' >Chillax</h1>
@@ -15,21 +15,21 @@ const About = () => {
                       <a  href='https://chillax.netlify.com'>Live</a>
                     </div>
                  <div className='container'>
-                     <span  className='container shadow   l1'>
+                     <span  className='container shadow l1'>
                       <i className='fa fa-calendar-check' style={{ marginRight: '10px', color: 'blue' }}></i>
                       First Released
                      </span>
                      <span  className='container shadow   l1' style={{  fontWeight:'bold' }}>
                      13 May 2018
                      </span>
-                    <div className='container shadow l2 '><i className='fa fa-github' /> <a href='https://github.com/at1208/Chillax'>https://github.com/at1208/Chillax</a></div>
+                    <div className='container shadow l2 '><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Chillax'>https://github.com/at1208/Chillax</a></div>
                  </div>
 
                 </div>
               </div>
 
 
-              <div className='col-lg-3 shadow container  text-center u1'>
+              <div className='col-lg-3 shadow container  text-center u1 o2'>
                 <img className='   i1' src='searchbar.png' alt=''/>
                 <div className='container text-center  k1'>
                  <h1 className='v1'>SearchBar</h1>
@@ -45,13 +45,13 @@ const About = () => {
                        <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                        18 May 2018
                         </span>
-                          <div className='container shadow l2'><i className='fa fa-github' /> <a href='https://github.com/at1208/SearchBar'>https://github.com/at1208/SearchBar</a></div>
+                          <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/SearchBar'>https://github.com/at1208/SearchBar</a></div>
                    </div>
                 </div>
               </div>
 
 
-              <div className='col-lg-3 shadow container  text-center u1'>
+              <div className='col-lg-3 shadow container  text-center u1 o3'>
                 <img className='    i1' src='shopnow.png' alt=''/>
                 <div className='container text-center  k1'>
                 <h1 className='v1'>Shopping</h1>
@@ -67,13 +67,13 @@ const About = () => {
                       <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                       31 May 2018
                        </span>
-                         <div className='container shadow l2'><i className='fa fa-github' /> <a href='https://github.com/at1208/Shopping'>https://github.com/at1208/Shopping</a></div>
+                         <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Shopping'>https://github.com/at1208/Shopping</a></div>
                   </div>
                 </div>
               </div>
 
 
-              <div className='col-lg-3 shadow container   text-center u1'>
+              <div className='col-lg-3 shadow container   text-center u1 o4'>
                 <img className='     i1' src='changelang.png' alt=''/>
                 <div className='container text-center  k1'>
                  <h1 className='v1'>Change Lang</h1>
@@ -89,13 +89,13 @@ const About = () => {
                        <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                        24 May 2018
                         </span>
-                          <div className='container shadow l2'><i className='fa fa-github' /> <a href='https://github.com/at1208/LangSelector'>https://github.com/at1208/LangSelector</a></div>
+                          <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/LangSelector'>https://github.com/at1208/LangSelector</a></div>
                    </div>
                 </div>
               </div>
 
 
-              <div className='col-lg-3 shadow container   text-center u1'>
+              <div className='col-lg-3 shadow container   text-center u1 o5'>
                 <img className='     i1' src='winsum.png' alt=''/>
                 <div className='container text-center  k1'>
                 <h1 className='v1'>Season</h1>
@@ -111,13 +111,13 @@ const About = () => {
                       <span  className='container shadow col-xm l1' style={{  fontWeight:'bold' }}>
                       18 May 2018
                        </span>
-                         <div className='container shadow l2'><i className='fa fa-github' /> <a href='https://github.com/at1208/Geolocation'>https://github.com/at1208/Geolocation</a></div>
+                         <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Geolocation'>https://github.com/at1208/Geolocation</a></div>
                   </div>
                 </div>
               </div>
 
 
-              <div className='col-lg-3  shadow container  text-center u1'>
+              <div className='col-lg-3  shadow container  text-center u1 o6'>
                 <img className='    i1' src='weather.png' alt=''/>
                 <div className='container text-center   k1'>
                   <h1 className='v1'>Weather Finder</h1>
@@ -133,7 +133,7 @@ const About = () => {
                         <span  className='container shadow col-xm l1' >
                         13 Feb 2018
                          </span>
-                           <div className='container shadow l2'><i className='fa fa-github' /> <a href='https://github.com/at1208/Weather-finder'>https://github.com/at1208/Weather-finder</a></div>
+                           <div className='container shadow l2'><i className='fa fa-github' style={{ fontSize: '14px'}}/> <a href='https://github.com/at1208/Weather-finder'>https://github.com/at1208/Weather-finder</a></div>
                     </div>
                 </div>
               </div>
