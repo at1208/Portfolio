b15052d60e10c6900cf0464a9e2d21592d07684c
aman tiwari <amantiwari12081999@gmail.com>
2019-07-07 18:19:40 +0530

    Updates

---
+++ precache-manifest.237922f044972654c2090d9500bba57e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5df8e729e2c138d882c923fbf8799a",
+    "url": "/amantiwari/index.html"
+  },
+  {
+    "revision": "12e9fcc9520a39613510",
+    "url": "/amantiwari/static/css/main.847c94d3.chunk.css"
+  },
+  {
+    "revision": "03ca40376ea416303b64",
+    "url": "/amantiwari/static/js/2.083e9f82.chunk.js"
+  },
+  {
+    "revision": "12e9fcc9520a39613510",
+    "url": "/amantiwari/static/js/main.580b6e20.chunk.js"
+  },
+  {
+    "revision": "1b5db5277d97d7e7d5de",
+    "url": "/amantiwari/static/js/runtime~main.e558a5c2.js"
+  }
+]);
