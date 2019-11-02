b6a4fba826af18f56ee7ccd28180c23b85aeabbb
at1208 <amantiwari12081999@gmail.com>
2019-11-02 09:53:17 +0530

    brand icons size increase

--- src/components/about/quora.js
+++ src/components/about/quora.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './about.css'
 
 const Quora = () => {
-  return  <i class="fa fa-quora s1" style={{ fontSize:'22px',color:' #AA2200'}}></i>
+  return  <i class="fa fa-quora s1" style={{ fontSize:'28px',color:' #AA2200'}}></i>
 }
 
 export default Quora;
